e51b6b6226b54e073d327f5821b54faf31d165e2
ccrlcy <ccrlcy@gmail.com>
2020-06-11 16:38:17 +0900

    main page ejs 편집

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #fang
+codeSandbox cmd 실행 명령어. 
+이걸 실행 안하면 에러남... 
+1. rm -rf node_modules
+2. yarn cache clean
+3. yarn
+4. yarn start
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser'); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,8 +14,8 @@ var app = express();
 // })
 
 
-app.get('/', (req, res) => { res.send('index page') });
-app.get('/test', (req, res) => { res.send('test page')});
+// app.get('/', (req, res) => { res.send('index page') });
+// app.get('/test', (req, res) => { res.send('test page')});
 
 
 // app.use('/', indexRouter);
@@ -23,18 +23,23 @@ app.get('/test', (req, res) => { res.send('test page')});
 
 
 app.listen(3000, () => {
-	console.log('3000번 port에 http')
+	console.log('3000번 port에 http 실행중')
 })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){
+	res.render('index')
+});
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 // catch 404 and forward to error handler
